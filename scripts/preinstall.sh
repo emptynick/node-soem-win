195be5424564a6635230ca50f95002469423f7ac
Christoph Schweppe <info@cschweppe.de>
2022-07-19 09:03:50 +0200

    Update preinstall.sh

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -7,9 +7,8 @@ cd lib
 sed -i '/target_link_libraries(soem\ ${OS_LIBS})/a \ \ set_property(TARGET soem PROPERTY POSITION_INDEPENDENT_CODE ON)' CMakeLists.txt
 mkdir build
 cd build
-cmake ..
-make
+cmake .. -G "NMake Makefiles"
+nmake
 cd ..
 #cp build/libsoem.so /usr/local/lib
 cd ..
-
